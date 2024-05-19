07e991d9dc71471ed038c2d133d5ed4b35cc4467
Triparna singha <153750421+codewalltrips@users.noreply.github.com>
2024-05-19 13:11:04 +0530

    Add files via upload

---
+++ BITWISE/AND.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ main()
+ {
+ int a = 12, b = 25;
+ printf("Output = %d", a&b);
+ 
+ }
---
+++ BITWISE/COMPLIMENT.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ main()
+ {
+ printf("Output = %d\n",~35);
+ printf("Output = %d\n",~-12);
+ 
+ }
